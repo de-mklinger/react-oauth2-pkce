166bf0bb57c938a29051f7bb5e224bcd343cf355
Marc Klinger <mklinger@mklinger.de>
2023-01-19 01:01:11 +0100

    style: Apply prettier

--- src/util.ts
+++ src/util.ts
@@ -1,16 +1,14 @@
-export const toSnakeCase = (str: string): string => str
+export const toSnakeCase = (str: string): string =>
+  str
     .split(/(?=[A-Z])/)
     .join("_")
     .toLowerCase();
 
-export const toUrlEncoded = (
-  obj: Record<string, string | undefined>
-): string => Object.keys(obj)
+export const toUrlEncoded = (obj: Record<string, string | undefined>): string =>
+  Object.keys(obj)
     .filter((k) => obj[k] !== undefined)
     .map(
       (k) =>
-        encodeURIComponent(toSnakeCase(k)) +
-        "=" +
-        encodeURIComponent(obj[k]!)
+        encodeURIComponent(toSnakeCase(k)) + "=" + encodeURIComponent(obj[k]!)
     )
     .join("&");
